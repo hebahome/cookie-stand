faeea698e5ea4ad6d7a545ab986635f16136c8be
hebahome <hebaaltaweel349@gmail.com>
2021-08-03 21:42:18 +0300

    initial work added for lab06

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+
+
+let seattleCookies  = {min: 23, max:65, average:6.3};
+
+
+let tokyoCookies  = {min: 3, max:24, average:1.2};
+
+
+let dubaiCookies  = {min: 11, max:38, average:3.7};
+
+let parisCookies  = {min: 20, max:38, average:2.3};
+
+let limaCookies   = {min: 2, max:16, average:4.6};
+
+
+
+
+
+
+
+
+
+
+
+
